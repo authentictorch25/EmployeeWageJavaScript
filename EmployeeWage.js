a74c858f517a3460c5942050968e96870efd5bd2
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-20 17:28:34 +0530

    [Akash]Performed UC7 on the jobjects

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -77,3 +77,58 @@ let noWorkingDays = empDailyHoursAndWageArr
     .map(obj => obj.toString());
 console.log("\nUC 11.b Show no working days");
 console.log(noWorkingDays);
+
+//UC 7.a(i) Use Array.foreach to calculate total emp wage
+let totalEmpWage1 = 0;
+empDailyHoursAndWageArr.forEach(function (obj) {
+    totalEmpWage1 += obj.dailyWage;
+});
+console.log("\nUC 7.a Total Wage: " + totalEmpWage1);
+
+//UC 7.a (ii) Use Array.reduce method to calculate total emp wage
+function sum(totalWage, obj) {
+    return totalWage + obj.dailyWage;
+}
+
+//UC 7.b show the day along with daily wage using Array.map function
+let dayCounter = 0;
+function MapDayWithWage(obj) {
+    ++dayCounter;
+    return dayCounter + " - " + obj.dailyWage;
+}
+let mapDayWithWageArr = empDailyHoursAndWageArr.map(MapDayWithWage);
+console.log("UC 7.b Day with wage");
+console.log(mapDayWithWageArr);
+
+//UC 7.c show days when full time wage was earned
+function CheckIfFullTimeWage(dayAndWage) {
+    return dayAndWage.includes("160");
+}
+let fullTimeWageArr = mapDayWithWageArr.filter(CheckIfFullTimeWage);
+console.log("UC 7.c Days when full time wage was earned")
+console.log(fullTimeWageArr);
+
+//UC 7.d find the first occurence when full time wage was used
+console.log("UC 7.d First day when when full time wage was earned");
+console.log(mapDayWithWageArr.find(CheckIfFullTimeWage));
+
+//UC 7.e Check if every element of full time wage is truly holding full time wage
+console.log("UC 7.e Check if fullTimeWageArr is truly holding full time wage");
+console.log(fullTimeWageArr.every(CheckIfFullTimeWage));
+
+//UC 7.f Check if there is any part time wage
+function CheckIfPartTimeWage(dayAndWage) {
+    return dayAndWage.includes("80");
+}
+console.log("UC 7.f Check of any part time wage was earned or not");
+console.log(mapDayWithWageArr.some(CheckIfPartTimeWage));
+
+// UC 7.g Find the number of days employee worked
+function WorkingDays(countOfWorkingDays, obj) {
+    if (obj.dailyWage > 0) {
+        countOfWorkingDays++;
+    }
+    return countOfWorkingDays;
+}
+let daysEmpWorked = empDailyHoursAndWageArr.reduce(WorkingDays, 0);
+console.log("UC 7.g Number of days employee worked: " + daysEmpWorked);
