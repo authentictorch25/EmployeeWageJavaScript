705f9774f2ef42c68efc7ffde27f281ab3e3d1b6
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-20 16:23:41 +0530

    [Akash]Added program to calculate given maximum hours and day condition

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,6 +6,9 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const WORKING_DAYS_IN_A_MONTH = 20; /*UC4*/
+const MAX_WORKING_DAYS = 20;/*UC5*/
+const MAX_WORKING_HOURS = 160;/*UC5*/
+
 let empCheck = Math.floor(Math.random() * 10) % 2;
 if (empCheck == IS_ABSENT) {
     console.log("Employee is ABSENT");
@@ -45,6 +48,7 @@ function GetWorkingHours(empCheck){
 }
 
 /*UC4*/
+/*
 let empHrs = 0;
 for (let days = 1; days <= WORKING_DAYS_IN_A_MONTH; days++) {
     let empCheck = Math.floor(Math.random() * 100) % 3;
@@ -52,4 +56,20 @@ for (let days = 1; days <= WORKING_DAYS_IN_A_MONTH; days++) {
 }
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Total Hours: " + empHrs + "\nTotal Wage: " + empWage);
+*/
+/*UC5*/
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_WORKING_HOURS && totalWorkingDays <= MAX_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 1000) % 3;
+    let dailyEmpHrs = GetWorkingHours(empCheck);
+    if (totalEmpHrs + dailyEmpHrs <= MAX_WORKING_HOURS) {
+        totalEmpHrs += dailyEmpHrs;
+    } else {
+        break;
+    }
+}
 
+let totalEmpWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Hours: " + totalEmpHrs + "\nTotal Wage: " + totalEmpWage);
